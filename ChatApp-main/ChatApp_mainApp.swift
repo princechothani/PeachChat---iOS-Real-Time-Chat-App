d25a22f519ba1c1d2c0b3ed3c4172c838631d7be
princechothani <158582498+princechothani@users.noreply.github.com>
2025-06-06 18:12:38 +0530

    Initial Commit

---
+++ ChatApp-main/ChatApp_mainApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp_mainApp.swift
+//  ChatApp-main
+//
+//  Created by Prince Chothani on 06/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp_mainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatApp-main/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatApp-main
+//
+//  Created by Prince Chothani on 06/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
